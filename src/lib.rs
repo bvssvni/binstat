bea12bcd2b389d960f5d01d3947ad68acf1982e2
Sven Nilsen <bvssvni@gmail.com>
2014-07-19 14:27:52 +0200

    Added `BinGraph`
    Closes https://github.com/bvssvni/binstat/issues/1

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,13 @@
 //! A library for modelling and reasoning about multidimensional
 //! binary states.
 
+use std::collections::Bitv;
+
+/// Models a multidimensional binary state.
+pub struct BinGraph<TAction> {
+    /// The states and the bits that can be flipped.
+    pub data: Vec<(Bitv, Bitv)>,
+    /// A description the action that takes when flipping bits.
+    pub actions: Vec<TAction>,
+}
 
